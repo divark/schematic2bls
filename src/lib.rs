f3c1306410412b3819f91d8204ae58ffe377a9d8
Tyler Schmidt <tmschmid@protonmail.com>
2023-11-04 12:28:20 -0700

    Added more unit tests and refined placement math.

--- src/lib.rs
+++ src/lib.rs
@@ -101,6 +101,7 @@ pub fn to_save_file_output(bricks: &Vec<Brick>) -> String {
 pub struct Brick {
     pub position: (f32, f32, f32),
     pub size: u32,
+    floored: bool,
 }
 
 #[derive(Clone, Copy)]
@@ -111,11 +112,16 @@ pub enum Direction {
     Right,
 }
 
+fn right_to_center_coord(right_coord: usize, size: u32) -> f32 {
+    right_coord as f32 - (size as f32 / 2.0)
+}
+
 impl Brick {
     pub fn new(size: u32) -> Brick {
         Brick {
             position: (0.0, 0.0, size as f32 / 4.0),
             size,
+            floored: true,
         }
     }
 
@@ -132,13 +138,18 @@ impl Brick {
     }
 
     pub fn from_right_coordinate(size: u32, right_position: (usize, usize, usize)) -> Brick {
+        let x = right_to_center_coord(right_position.0, size);
+        let y = right_to_center_coord(right_position.1, size);
+        let floored = right_position.2 == size as usize;
+
         Brick {
             position: (
-                (right_position.0 as f32 / 2.0) - (size as f32 / 2.0),
-                (right_position.1 as f32 / 2.0) - (size as f32 / 2.0),
+                (x / 2.0) - (size as f32 / 4.0),
+                (y / 2.0) - (size as f32 / 4.0),
                 (right_position.2 as f32 / 2.0) - (size as f32 / 4.0),
             ),
             size,
+            floored,
         }
     }
 }
@@ -147,8 +158,8 @@ impl Display for Brick {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "{}x Cube\" {} {} {} 0 1 0  0 0 1 1 1",
-            self.size, self.position.0, self.position.1, self.position.2
+            "{}x Cube\" {} {} {} 0 {} 0  0 0 1 1 1",
+            self.size, self.position.0, self.position.1, self.position.2, self.floored as usize
         )
     }
 }
@@ -161,6 +172,23 @@ mod tests {
     fn place_one_4x_cube() {
         let expected = include_str!("../assets/brick_comparisons/4xCube.bls").to_string();
         let actual = to_save_file_output(&vec![Brick::new(4)]);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn place_4x_cube_tower() {
+        let size = 4;
+
+        let mut bricks = Vec::new();
+        for i in 1..=size {
+            bricks.push(Brick::from_right_coordinate(
+                size,
+                (size as usize, size as usize, size as usize * i as usize),
+            ));
+        }
+        let expected = include_str!("../assets/brick_comparisons/4xCubesTower.bls").to_string();
+        let actual = to_save_file_output(&bricks);
 
         assert_eq!(expected, actual);
     }
@@ -201,6 +229,8 @@ mod tests {
 
     #[test]
     fn place_1x4_4x_cubes() {
+        let size = 4;
+
         let desired_x_coordinate = 4;
         let desired_y_coordinates = vec![4, 8, 12, 16];
         let desired_z_coordinate = 4;
@@ -208,7 +238,7 @@ mod tests {
         let mut bricks = Vec::new();
         for y_coord in desired_y_coordinates {
             bricks.push(Brick::from_right_coordinate(
-                4,
+                size,
                 (desired_x_coordinate, y_coord, desired_z_coordinate),
             ));
         }
@@ -228,9 +258,87 @@ mod tests {
     }
 
     #[test]
+    fn place_1x4_8x_cubes() {
+        let size = 8;
+
+        let desired_x_coordinate = 8;
+        let desired_y_coordinates = vec![8, 16, 24, 32];
+        let desired_z_coordinate = 8;
+
+        let mut bricks = Vec::new();
+        for y_coord in desired_y_coordinates {
+            bricks.push(Brick::from_right_coordinate(
+                size,
+                (desired_x_coordinate, y_coord, desired_z_coordinate),
+            ));
+        }
+
+        let expected = include_str!("../assets/brick_comparisons/8xCubesLine.bls").to_string();
+        let actual = to_save_file_output(&bricks);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn place_8x_cube_tower() {
+        let size = 8;
+
+        let mut bricks = Vec::new();
+        for i in 1..=4 {
+            bricks.push(Brick::from_right_coordinate(
+                size,
+                (size as usize, size as usize, size as usize * i as usize),
+            ));
+        }
+        let expected = include_str!("../assets/brick_comparisons/8xCubesTower.bls").to_string();
+        let actual = to_save_file_output(&bricks);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
     fn place_one_16x_cube() {
         let expected = include_str!("../assets/brick_comparisons/16xCube.bls").to_string();
         let actual = to_save_file_output(&vec![Brick::new(16)]);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn place_1x4_16x_cubes() {
+        let size = 16;
+
+        let desired_x_coordinate = 16;
+        let desired_y_coordinates = vec![16, 32, 48, 64];
+        let desired_z_coordinate = 16;
+
+        let mut bricks = Vec::new();
+        for y_coord in desired_y_coordinates {
+            bricks.push(Brick::from_right_coordinate(
+                size,
+                (desired_x_coordinate, y_coord, desired_z_coordinate),
+            ));
+        }
+
+        let expected = include_str!("../assets/brick_comparisons/16xCubesLine.bls").to_string();
+        let actual = to_save_file_output(&bricks);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn place_16x_cube_tower() {
+        let size = 16;
+
+        let mut bricks = Vec::new();
+        for i in 1..=4 {
+            bricks.push(Brick::from_right_coordinate(
+                size,
+                (size as usize, size as usize, size as usize * i as usize),
+            ));
+        }
+        let expected = include_str!("../assets/brick_comparisons/16xCubesTower.bls").to_string();
+        let actual = to_save_file_output(&bricks);
 
         assert_eq!(expected, actual);
     }
@@ -244,9 +352,103 @@ mod tests {
     }
 
     #[test]
+    fn place_1x4_32x_cubes() {
+        let size = 32;
+
+        let desired_x_coordinate = 32;
+        let desired_y_coordinates = vec![32, 64, 96, 128];
+        let desired_z_coordinate = 32;
+
+        let mut bricks = Vec::new();
+        for y_coord in desired_y_coordinates {
+            bricks.push(Brick::from_right_coordinate(
+                size,
+                (desired_x_coordinate, y_coord, desired_z_coordinate),
+            ));
+        }
+
+        let expected = include_str!("../assets/brick_comparisons/32xCubesLine.bls").to_string();
+        let actual = to_save_file_output(&bricks);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn place_32x_cube_tower() {
+        let size = 32;
+
+        let mut bricks = Vec::new();
+        for i in 1..=4 {
+            bricks.push(Brick::from_right_coordinate(
+                size,
+                (size as usize, size as usize, size as usize * i as usize),
+            ));
+        }
+        let expected = include_str!("../assets/brick_comparisons/32xCubesTower.bls").to_string();
+        let actual = to_save_file_output(&bricks);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
     fn place_one_64x_cube() {
         let expected = include_str!("../assets/brick_comparisons/64xCube.bls").to_string();
         let actual = to_save_file_output(&vec![Brick::new(64)]);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn place_1x4_64x_cubes() {
+        let size = 64;
+
+        let desired_x_coordinate = 64;
+        let desired_y_coordinates = vec![64, 128, 192, 256];
+        let desired_z_coordinate = 64;
+
+        let mut bricks = Vec::new();
+        for y_coord in desired_y_coordinates {
+            bricks.push(Brick::from_right_coordinate(
+                size,
+                (desired_x_coordinate, y_coord, desired_z_coordinate),
+            ));
+        }
+
+        let expected = include_str!("../assets/brick_comparisons/64xCubesLine.bls").to_string();
+        let actual = to_save_file_output(&bricks);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn place_64x_cube_tower() {
+        let size = 64;
+
+        let mut bricks = Vec::new();
+        for i in 1..=4 {
+            bricks.push(Brick::from_right_coordinate(
+                size,
+                (size as usize, size as usize, size as usize * i as usize),
+            ));
+        }
+        let expected = include_str!("../assets/brick_comparisons/64xCubesTower.bls").to_string();
+        let actual = to_save_file_output(&bricks);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn place_increasing_cubes_scale() {
+        let expected = include_str!("../assets/brick_comparisons/CubeScale.bls").to_string();
+
+        let bricks = vec![
+            Brick::from_right_coordinate(4, (4, 4, 4)),
+            Brick::from_right_coordinate(8, (12, 8, 8)),
+            Brick::from_right_coordinate(16, (28, 16, 16)),
+            Brick::from_right_coordinate(32, (60, 32, 32)),
+            Brick::from_right_coordinate(64, (124, 64, 64)),
+        ];
+        let actual = to_save_file_output(&bricks);
 
         assert_eq!(expected, actual);
     }
--- src/main.rs
+++ src/main.rs
@@ -7,75 +7,86 @@ use std::io::Write;
 use std::path::Path;
 
 fn main() {
-    let command_args: Vec<String> = env::args().collect();
+    // let command_args: Vec<String> = env::args().collect();
 
-    if command_args.len() != 2 {
-        eprintln!("Usage: {} schematic_file_path", command_args[0]);
-        return;
-    }
+    // if command_args.len() != 2 {
+    //     eprintln!("Usage: {} schematic_file_path", command_args[0]);
+    //     return;
+    // }
 
-    let file_path = Path::new(&command_args[1]);
-    if !file_path.exists() {
-        eprintln!(
-            "{}: File {} was not found.",
-            command_args[0], command_args[1]
-        );
-        return;
-    }
+    // let file_path = Path::new(&command_args[1]);
+    // if !file_path.exists() {
+    //     eprintln!(
+    //         "{}: File {} was not found.",
+    //         command_args[0], command_args[1]
+    //     );
+    //     return;
+    // }
 
-    let mut file_cursor =
-        Cursor::new(fs::read(file_path).expect("grid2bls: Could not read file into bytes."));
+    // let mut file_cursor =
+    //     Cursor::new(fs::read(file_path).expect("grid2bls: Could not read file into bytes."));
 
-    let schematic_tag = read_gzip_compound_tag(&mut file_cursor);
-    if schematic_tag.is_err() {
-        eprintln!(
-            "{}: Invalid schematic file given from file {}",
-            command_args[0], command_args[1]
-        );
-        return;
-    }
+    // let schematic_tag = read_gzip_compound_tag(&mut file_cursor);
+    // if schematic_tag.is_err() {
+    //     eprintln!(
+    //         "{}: Invalid schematic file given from file {}",
+    //         command_args[0], command_args[1]
+    //     );
+    //     return;
+    // }
 
-    let schematic_tag = schematic_tag.expect("grid2bls: A Compound Tag should exist by now.");
-    let three_dimensional_grid = schematic_to_3dgrid(schematic_tag);
-    let largest_cubes = grid_to_largest_cubes(three_dimensional_grid);
+    // let schematic_tag = schematic_tag.expect("grid2bls: A Compound Tag should exist by now.");
+    // let three_dimensional_grid = schematic_to_3dgrid(schematic_tag);
+    // let largest_cubes = grid_to_largest_cubes(three_dimensional_grid);
+
+    // let mut bricks = Vec::new();
+    let size = 8;
 
     let mut bricks = Vec::new();
-    while let Some(found_largest_cube) = get_largest_cube(&largest_cubes) {
-        let parsed_brick = Brick::from_right_coordinate(
-            found_largest_cube.side_length as u32,
-            found_largest_cube.indexes,
-        );
-        bricks.push(parsed_brick);
+    for i in 1..=size {
+        bricks.push(Brick::from_right_coordinate(
+            size,
+            (size as usize, size as usize, size as usize * i as usize),
+        ));
     }
+    // while let Some(found_largest_cube) = get_largest_cube(&largest_cubes) {
+    //     let parsed_brick = Brick::from_right_coordinate(
+    //         found_largest_cube.side_length as u32,
+    //         found_largest_cube.indexes,
+    //     );
+    //     bricks.push(parsed_brick);
+    // }
 
     let save_file_contents = to_save_file_output(&bricks);
 
-    let mut output_file_name = String::from(
-        file_path
-            .file_stem()
-            .expect("grid2bls: File does not have a stem, huh.")
-            .to_str()
-            .expect("grid2bls: Could not convert OSString to str."),
-    );
-    output_file_name.push_str(".bls");
+    println!("{}", save_file_contents.to_string());
 
-    let output = File::create(&output_file_name);
-    if let Err(msg) = output {
-        eprintln!(
-            "{}: Could not write output file {}: {}",
-            command_args[0], output_file_name, msg
-        );
-        return;
-    }
+    // let mut output_file_name = String::from(
+    //     file_path
+    //         .file_stem()
+    //         .expect("grid2bls: File does not have a stem, huh.")
+    //         .to_str()
+    //         .expect("grid2bls: Could not convert OSString to str."),
+    // );
+    // output_file_name.push_str(".bls");
 
-    let mut output_file = output.expect("grid2bls: File should be created by now.");
+    // let output = File::create(&output_file_name);
+    // if let Err(msg) = output {
+    //     eprintln!(
+    //         "{}: Could not write output file {}: {}",
+    //         command_args[0], output_file_name, msg
+    //     );
+    //     return;
+    // }
 
-    if let Err(msg) = output_file.write_all(save_file_contents.as_bytes()) {
-        eprintln!(
-            "{}: Could not write to newly created output file {}: {}",
-            command_args[0], output_file_name, msg
-        );
-    }
+    // let mut output_file = output.expect("grid2bls: File should be created by now.");
+
+    // if let Err(msg) = output_file.write_all(save_file_contents.as_bytes()) {
+    //     eprintln!(
+    //         "{}: Could not write to newly created output file {}: {}",
+    //         command_args[0], output_file_name, msg
+    //     );
+    // }
 }
 
 pub fn schematic_to_3dgrid(schematic_root: CompoundTag) -> Vec<Vec<Vec<bool>>> {
