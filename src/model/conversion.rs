65da9200718865e12314e3ef7cfe7b41738abfa1
Tyler Schmidt <tmschmid@protonmail.com>
2023-12-18 11:47:08 -0800

    Fix three dimensional to one dimensional indexing issue.

--- src/model/conversion.rs
+++ src/model/conversion.rs
@@ -1,5 +1,9 @@
 use nbt::CompoundTag;
 
+fn three_to_one_dim_idx(x: usize, y: usize, z: usize, length: usize, width: usize) -> usize {
+    (z * length * width) + (width * x) + y
+}
+
 pub fn schematic_to_3dgrid(schematic_root: CompoundTag) -> Vec<Vec<Vec<bool>>> {
     let length = schematic_root
         .get_i16("Length")
@@ -11,16 +15,17 @@ pub fn schematic_to_3dgrid(schematic_root: CompoundTag) -> Vec<Vec<Vec<bool>>> {
         .get_i16("Height")
         .expect("Could not get Height field") as usize;
 
-    let mut grid = vec![vec![vec![false; height]; width]; length];
+    let mut grid = vec![vec![vec![false; width]; height]; length];
 
     let blocks = schematic_root
         .get_i8_vec("Blocks")
         .expect("Could not get Blocks field in schematic.");
-    for (i, length_entry) in grid.iter_mut().enumerate() {
-        for (j, width_entry) in length_entry.iter_mut().enumerate() {
-            for (k, height_entry) in width_entry.iter_mut().enumerate() {
-                let blocks_index = (j * length + k) * width + i;
-                *height_entry = blocks[blocks_index] == 1;
+    for i in 0..length {
+        for j in 0..width {
+            for k in 0..height {
+                let blocks_index = three_to_one_dim_idx(i, j, k, length, width);
+
+                grid[i][k][j] = blocks[blocks_index] == 1;
             }
         }
     }
@@ -36,8 +41,7 @@ mod tests {
     use std::io::Cursor;
 
     fn load_test_schematic() -> CompoundTag {
-        let mut file_cursor =
-            Cursor::new(include_bytes!("../../assets/peachs_castle.schematic").to_vec());
+        let mut file_cursor = Cursor::new(include_bytes!("../../assets/peachs_castle.schematic").to_vec());
         read_gzip_compound_tag(&mut file_cursor).expect("Could not read given schematic file.")
     }
 
@@ -55,6 +59,29 @@ mod tests {
         }
 
         num_blocks
+    }
+
+    #[test]
+    fn three_to_one_dimension_indexes_within_bounds() {
+        let length = 4;
+        let width = 3;
+        let height = 2;
+
+        let expected_indexes = (0..(length * width * height)).collect::<Vec<usize>>();
+
+        let mut actual_indexes = Vec::new();
+        for i in 0..length {
+            for j in 0..width {
+                for k in 0..height {
+                    let actual_index = three_to_one_dim_idx(i, j, k, length, width);
+                    actual_indexes.push(actual_index);
+                }
+            }
+        }
+
+        actual_indexes.sort_unstable();
+
+        assert_eq!(expected_indexes, actual_indexes);
     }
 
     #[test]
