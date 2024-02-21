0138550b26800a914b476c1aecc235ea78ab6ae7
Tyler Schmidt <tmschmid@protonmail.com>
2024-02-20 16:00:09 -0800

    Fix schematic2bls not running due to invalid number of arguments every time.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use std::{env, fs::File, io::Write};
 
 fn main() {
     let execution_args: Vec<String> = env::args().collect();
-    if execution_args.len() != 2 || execution_args.len() != 3 {
+    if execution_args.len() < 2 || execution_args.len() > 3 {
         eprintln!("Usage: schematic2bls <path_to_schematic> [scaling_factor]");
         return;
     }
 
-    let model_arg = execution_args.get(1).expect("schematic2bls: path_to_schematic not provided.");
+    let model_arg = execution_args
+        .get(1)
+        .expect("schematic2bls: path_to_schematic not provided.");
     let scaling_factor = if let Some(scaling_arg) = execution_args.get(2) {
         scaling_arg.parse::<u8>().unwrap_or(1)
     } else {
